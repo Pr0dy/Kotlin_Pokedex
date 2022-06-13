6d9accf8d8472f5db28ea9588d950d421d5f94b0
Pr0dy <leandro.miguel.banha@gmail.com>
2022-06-13 15:01:33 +0100

    basic login implemented!

--- app/src/main/java/com/app/pokedex_45486_44499/Activities/LoginActivity.kt
+++ app/src/main/java/com/app/pokedex_45486_44499/Activities/LoginActivity.kt
@@ -1,42 +1,61 @@
 package com.app.pokedex_45486_44499.Activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.app.pokedex_45486_44499.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    var username: EditText? = null
+    var password: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        mAuth = Firebase.auth
+        username = findViewById(R.id.editTextTextPersonName)
+        password = findViewById(R.id.editTextTextPassword)
         setup()
     }
 
     private fun setup(){
         findViewById<Button>(R.id.btn_auth).setOnClickListener {
 
-            val username = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+            val usernameStr = findViewById<EditText>(R.id.editTextTextPersonName).text.toString().trim()
+            val passwordStr = findViewById<EditText>(R.id.editTextTextPassword).text.toString().trim()
 
-            if (username.isEmpty()) {
-                //findViewById<TextView>(R.id.tv_error).visibility = View.VISIBLE
-            }
-            val password = findViewById<EditText>(R.id.editTextTextPassword).text.toString()
-
-            if (password.isEmpty()) {
-                //findViewById<TextView>(R.id.tv_error).visibility = View.VISIBLE
+            if(usernameStr.isEmpty()){
+                username?.setError("Email can't be empty!")
+                return@setOnClickListener
             }
 
-            //firebase
+            if (passwordStr.isEmpty()){
+                password?.setError("Password can't be empty!")
+                return@setOnClickListener
+            }
 
-            //val isValid = username == password
-            //if (isValid) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            //}
+            mAuth.signInWithEmailAndPassword(usernameStr,passwordStr).addOnCompleteListener(
+                OnCompleteListener {
+                    if(it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Failed to login! Account does not exist, try again!", Toast.LENGTH_LONG).show()
+                    }
+                })
         }
     }
+
+
 }
